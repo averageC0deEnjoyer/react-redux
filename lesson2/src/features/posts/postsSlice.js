ea758f12d86518afa031f97af90063a9c600d86a
averageC0deEnjoyer <136006438+averageC0deEnjoyer@users.noreply.github.com>
2024-01-06 15:22:08 +0700

    initialize for create async thunk

--- lesson2/src/features/posts/postsSlice.js
+++ lesson2/src/features/posts/postsSlice.js
@@ -1,35 +1,22 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { nanoid } from '@reduxjs/toolkit';
+import { createSlice, nanoid, createAsyncThunk } from '@reduxjs/toolkit';
 import { sub } from 'date-fns';
+import axios from 'axios';
+const POSTS_URL = 'https://jsonplaceeholder.typicode.com/posts';
 
-const initialState = [
-  {
-    id: '1',
-    title: 'lorem ipsum',
-    content: 'lorem ipsum',
-    date: sub(new Date(), { minutes: 10 }).toISOString(),
-    reactions: {
-      thumbsUp: 0,
-      wow: 0,
-      heart: 0,
-      rocket: 0,
-      coffee: 0,
-    },
-  },
-  {
-    id: '2',
-    title: 'lorem ipsum',
-    content: 'lorem ipsum',
-    date: sub(new Date(), { minutes: 5 }).toISOString(),
-    reactions: {
-      thumbsUp: 0,
-      wow: 0,
-      heart: 0,
-      rocket: 0,
-      coffee: 0,
-    },
-  },
-];
+const initialState = {
+  posts: [],
+  status: 'idle', //idle/loading/succeeded/failed
+  error: null,
+};
+
+export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
+  try {
+    const response = await axios.get(POSTS_URL);
+    return response.data;
+  } catch (err) {
+    return err.message;
+  }
+});
 
 const postsSlice = createSlice({
   name: 'posts',
@@ -37,7 +24,7 @@ const postsSlice = createSlice({
   reducers: {
     postAdded: {
       reducer(state, action) {
-        state.push(action.payload);
+        state.posts.push(action.payload);
       },
       prepare(title, content, userId) {
         return {
@@ -59,9 +46,8 @@ const postsSlice = createSlice({
       },
     },
     reactionAdded(state, action) {
-      console.log(action);
       const { postId, reaction } = action.payload;
-      const existingPost = state.find((post) => post.id === postId);
+      const existingPost = state.posts.find((post) => post.id === postId);
       if (existingPost) {
         existingPost.reactions[reaction]++;
       }
@@ -70,7 +56,7 @@ const postsSlice = createSlice({
 });
 
 //to be used in useSelector hooks (so it have the state/store params)
-export const selectAllPosts = (store) => store.posts;
+export const selectAllPosts = (store) => store.posts.posts;
 
 export const { postAdded, reactionAdded } = postsSlice.actions;
 
